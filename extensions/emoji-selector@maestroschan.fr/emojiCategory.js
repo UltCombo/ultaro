7dd7a1a0c3abab1c9df88fba4de3de0945132133
UltCombo <ultcombo@gmail.com>
2019-03-23 23:42:47 -0300

    feat: Update extensions

---
+++ extensions/emoji-selector@maestroschan.fr/emojiCategory.js
@@ -0,0 +1,126 @@
+//this file is part of https://github.com/maoschanz/emoji-selector-for-gnome
+
+const St = imports.gi.St;
+const PopupMenu = imports.ui.popupMenu;
+
+/* Import the current extension, mainly because we need to access other files */
+const ExtensionUtils = imports.misc.extensionUtils;
+const Me = ExtensionUtils.getCurrentExtension();
+const SkinTonesBar = Me.imports.emojiOptionsBar.SkinTonesBar;
+const Extension = Me.imports.extension;
+const EmojiButton = Me.imports.emojiButton;
+
+//class EmojiCategory
+//methods :
+//	constructor(string, string, int)	init the button & the submenu's menu-item (label, tone/gender)
+//	clear()								remove all emojis from the category
+//	build()								build category's rows and buttons in it
+//	_toggle()							do everything needed when the user click on the category's button
+//	_openCategory()						open the category
+//	getButton()							not useful getter
+var EmojiCategory = class EmojiCategory {
+	constructor(categoryName, iconName, id) {
+		this.super_item = new PopupMenu.PopupSubMenuMenuItem(categoryName);
+		this.categoryName = categoryName;
+		this.id = id;
+
+		this.super_item.actor.visible = false;
+		this.super_item.actor.reactive = false;
+		this.super_item._triangleBin.visible = false;
+
+		this.emojiButtons = []; // used only for updating the size/style
+
+		// A single widget is created for all categories to simplify the update method
+		if ((this.id == 1) || (this.id == 5)) {
+			this.skinTonesBar = new SkinTonesBar(true);
+		} else {
+			this.skinTonesBar = new SkinTonesBar(false);
+		}
+
+		//	Smileys & body		Peoples			Activities
+		if ((this.id == 0) || (this.id == 1) || (this.id == 5)) {
+			this.skinTonesBar.addBar(this.super_item.actor);
+		}
+
+		this.categoryButton = new St.Button({
+			reactive: true,
+			can_focus: true,
+			track_hover: true,
+			accessible_name: categoryName,
+			style_class: 'system-menu-action',
+			child: new St.Icon({ icon_name: iconName }),
+		});
+		this.categoryButton.connect('clicked', this._toggle.bind(this));
+		this.categoryButton.connect('notify::hover', this._onHover.bind(this));
+
+		this._built = false; // will be true once loaded
+	}
+
+	clear() {
+		this.super_item.menu.removeAll();
+		this.emojiButtons = [];
+	}
+
+	build() { // load the category XXX
+		let ln, container;
+		for (let i = 0; i < Extension.EMOJIS_CHARACTERS[this.id].length; i++) {
+			// lines of emojis
+			if (i % Extension.NB_COLS === 0) {
+				ln = new PopupMenu.PopupBaseMenuItem({
+					style_class: 'EmojisList',
+					reactive: false,
+					can_focus: false,
+				});
+				ln.actor.track_hover = false;
+				container = new St.BoxLayout();
+				ln.actor.add(container, { expand: true });
+				this.super_item.menu.addMenuItem(ln);
+			}
+
+			let button = new EmojiButton.EmojiButton(Extension.EMOJIS_CHARACTERS[this.id][i],
+			                       this, Extension.EMOJIS_KEYWORDS[this.id][i]);
+			this.emojiButtons.push(button);
+			container.add_child(button.super_btn);
+		}
+		this._built = true;
+	}
+
+	_toggle() {
+		if (this.super_item._getOpenState()) {
+			Extension.GLOBAL_BUTTON.clearCategories();
+		} else {
+			this._openCategory();
+		}
+	}
+
+	_onHover(a, b) {
+		if (a.hover) {
+			this.categoryButton.style = 'background-color: rgba(200, 200, 200, 0.2);';
+		} else if (this.super_item._getOpenState()) {
+			this.categoryButton.style = 'background-color: rgba(0, 0, 100, 0.2);';
+		} else {
+			this.categoryButton.style = '';
+		}
+	}
+
+	_openCategory() {
+		Extension.GLOBAL_BUTTON.clearCategories();
+		this.super_item.label.text = this.categoryName;
+
+		if(!this._built) {
+			this.build();
+		}
+		this.skinTonesBar.update();
+
+		this.categoryButton.style = 'background-color: rgba(0, 0, 200, 0.2);';
+		this.super_item.actor.visible = true;
+		this.super_item.setSubmenuShown(true);
+		Extension.GLOBAL_BUTTON._activeCat = this.id;
+		Extension.GLOBAL_BUTTON._onSearchTextChanged();
+	}
+
+	getButton() {
+		return this.categoryButton;
+	}
+}
+
--- extensions/emoji-selector@maestroschan.fr/prefs.js
+++ extensions/emoji-selector@maestroschan.fr/prefs.js
@@ -4,8 +4,6 @@ const GObject = imports.gi.GObject;
 const Gio = imports.gi.Gio;
 const Gtk = imports.gi.Gtk;
 const GdkPixbuf = imports.gi.GdkPixbuf;
-const Lang = imports.lang;
-
 const Mainloop = imports.mainloop;
 
 const Gettext = imports.gettext.domain('emoji-selector');
@@ -23,16 +21,14 @@ function init() {
 
 //-----------------------------------------------
 
-const PrefsPage = new Lang.Class({
-	Name: "PrefsPage",
-	Extends: Gtk.ScrolledWindow,
-
-	_init: function () {
-		this.parent({
+const EmojiSelector_PrefsPage = GObject.registerClass(
+class EmojiSelector_PrefsPage extends Gtk.ScrolledWindow {
+	_init() {
+		super._init({
 			vexpand: true,
 			can_focus: true
 		});
-		
+
 		this.stackpageMainBox = new Gtk.Box({
 			visible: true,
 			can_focus: false,
@@ -41,12 +37,12 @@ const PrefsPage = new Lang.Class({
 			margin_top: 20,
 			margin_bottom: 20,
 			orientation: Gtk.Orientation.VERTICAL,
-			spacing: 18
+			spacing: 12
 		});
 		this.add(this.stackpageMainBox);
-	},
-	
-	add_section: function(titre) {
+	}
+
+	add_section(titre) {
 		let section = new Gtk.Box({
 			orientation: Gtk.Orientation.VERTICAL,
 			spacing: 6,
@@ -58,49 +54,49 @@ const PrefsPage = new Lang.Class({
 				use_markup: true,
 			}));
 		}
-	
+
+		let frame = new Gtk.Frame();
 		let a = new Gtk.ListBox({
 			can_focus: true,
 			selection_mode: Gtk.SelectionMode.NONE,
 		});
-		section.add(a);
+		frame.add(a)
+		section.add(frame);
 		this.stackpageMainBox.add(section);
 		return a;
-	},
+	}
 
-	add_row: function(filledbox, section) {
+	add_row(filledbox, section) {
 		let a = new Gtk.ListBoxRow({
 			can_focus: true,
-			selectable: false,	
+			selectable: false,
 		});
 		a.add(filledbox);
 		section.add(a);
 		return a;
-	},
-	
-	add_widget: function(filledbox) {
+	}
+
+	add_widget(filledbox) {
 		this.stackpageMainBox.add(filledbox);
-	},
+	}
 });
 
 //--------------------
 
-const EmojiPrefsWidget = new Lang.Class({
-	Name: "EmojiPrefsWidget",
-	Extends: Gtk.Stack,
-	
-	_init: function () {
-		this.parent({transition_type: Gtk.StackTransitionType.SLIDE_LEFT_RIGHT});
-		
+const EmojiSelector_PrefsWidget = GObject.registerClass(
+class EmojiSelector_PrefsWidget extends Gtk.Stack {
+	_init() {
+		super._init({transition_type: Gtk.StackTransitionType.SLIDE_LEFT_RIGHT});
+
 		this.switcher = new Gtk.StackSwitcher({
 			halign: Gtk.Align.CENTER,
 			stack: this
 		});
 		this.switcher.show_all();
-	},
-	
-	add_page: function (id, title) {
-		let page = new PrefsPage();
+	}
+
+	add_page(id, title) {
+		let page = new EmojiSelector_PrefsPage();
 		this.add_titled(page, id, title);
 		return page;
 	}
@@ -109,32 +105,36 @@ const EmojiPrefsWidget = new Lang.Class({
 let SETTINGS = Convenience.getSettings();
 
 function buildPrefsWidget() {
-	let widget = new EmojiPrefsWidget();
-	
-	let settingsPage = widget.add_page("settings", _("Settings"));	
-	
+	let widget = new EmojiSelector_PrefsWidget();
+
+	let settingsPage = widget.add_page("settings", _("Settings"));
+
 	let layoutSection = settingsPage.add_section(_("Layout"));
 	let appearanceSection = settingsPage.add_section(_("Appearance"));
 	let keybindingSection = settingsPage.add_section(_("Keybinding"));
-	
+
 	let RELOAD_TEXT = _("Modifications will be effective after reloading the extension.");
-	
+
 		//-------------------------------------------------
-		
+
 		let emojiSizeLabel = _("Size of emojis (px):");
-		
+
 		let emojiSize = new Gtk.SpinButton();
 		emojiSize.set_sensitive(true);
 		emojiSize.set_range(10, 100);
 		emojiSize.set_value(32);
 		emojiSize.set_value(SETTINGS.get_int('emojisize'));
 		emojiSize.set_increments(1, 2);
-		
-		emojiSize.connect('value-changed', Lang.bind(this, function(w){
+
+		emojiSize.connect('value-changed', w => {
 			var value = w.get_value_as_int();
 			SETTINGS.set_int('emojisize', value);
-		}));
-		
+		});
+
+		SETTINGS.connect('changed::emojisize', () => {
+			emojiSize.set_value(SETTINGS.get_int('emojisize'));
+		});
+
 		let sizeBox = new Gtk.Box({
 			orientation: Gtk.Orientation.HORIZONTAL,
 			spacing: 15,
@@ -142,22 +142,26 @@ function buildPrefsWidget() {
 		});
 		sizeBox.pack_start(new Gtk.Label({ label: emojiSizeLabel, halign: Gtk.Align.START }), false, false, 0);
 		sizeBox.pack_end(emojiSize, false, false, 0);
-		
+
 		//------------------------------------------------
-		
+
 		let lightThemeLabel = _("Use darker font:");
 		let lightThemeSwitch = new Gtk.Switch();
 		lightThemeSwitch.set_state(false);
 		lightThemeSwitch.set_state(SETTINGS.get_boolean('light-theme'));
-		
-		lightThemeSwitch.connect('notify::active', Lang.bind(this, function(widget) {
+
+		lightThemeSwitch.connect('notify::active', widget => {
 			if (widget.active) {
 				SETTINGS.set_boolean('light-theme', true);
 			} else {
 				SETTINGS.set_boolean('light-theme', false);
 			}
-		}));
-		
+		});
+
+		SETTINGS.connect('changed::light-theme', () => {
+			lightThemeSwitch.set_state(SETTINGS.get_boolean('light-theme'));
+		});
+
 		let lightThemeBox = new Gtk.Box({
 			orientation: Gtk.Orientation.HORIZONTAL,
 			spacing: 15,
@@ -166,27 +170,31 @@ function buildPrefsWidget() {
 		});
 		lightThemeBox.pack_start(new Gtk.Label({ label: lightThemeLabel, halign: Gtk.Align.START }), false, false, 0);
 		lightThemeBox.pack_end(lightThemeSwitch, false, false, 0);
-		
+
 		//------------------------------------------------
-		
+
 		let positionLabel = _("General layout:");
-		
+
 		let positionCombobox = new Gtk.ComboBoxText({
 			visible: true,
 			can_focus: false,
 			halign: Gtk.Align.END,
 			valign: Gtk.Align.CENTER
 		});
-		
+
 		positionCombobox.append('top', _("From top to bottom"));
 		positionCombobox.append('bottom', _("From bottom to top"));
-		
+
 		positionCombobox.active_id = SETTINGS.get_string('position');
-		
+
 		positionCombobox.connect("changed", (widget) => {
 			SETTINGS.set_string('position', widget.get_active_id());
 		});
-		
+
+		SETTINGS.connect('changed::position', () => {
+			positionCombobox.set_active_id(SETTINGS.get_string('position'));
+		});
+
 		let positionBox = new Gtk.Box({
 			orientation: Gtk.Orientation.HORIZONTAL,
 			spacing: 15,
@@ -197,66 +205,74 @@ function buildPrefsWidget() {
 		positionBox.pack_end(positionCombobox, false, false, 0);
 
 		//-------------------------------------
-		
+
 		let nbColsLabel = _("Number of emojis per line:");
-		
-		let nbCols = new Gtk.SpinButton();
-		nbCols.set_sensitive(true);
-		nbCols.set_range(1, 60);
-		nbCols.set_value(11);
-		nbCols.set_value(SETTINGS.get_int('nbcols'));
-		nbCols.set_increments(1, 2);
-		
-		nbCols.connect('value-changed', Lang.bind(this, function(w){
+
+		let nbColsSpinBtn = new Gtk.SpinButton();
+		nbColsSpinBtn.set_sensitive(true);
+		nbColsSpinBtn.set_range(1, 60);
+		nbColsSpinBtn.set_value(11);
+		nbColsSpinBtn.set_value(SETTINGS.get_int('nbcols'));
+		nbColsSpinBtn.set_increments(1, 2);
+
+		nbColsSpinBtn.connect('value-changed', w => {
 			var value = w.get_value_as_int();
 			SETTINGS.set_int('nbcols', value);
-		}));
-		
+		});
+
+		SETTINGS.connect('changed::nbcols', () => {
+			nbColsSpinBtn.set_value(SETTINGS.get_int('nbcols'));
+		});
+
 		let colsBox =  new Gtk.Box({
 			orientation: Gtk.Orientation.HORIZONTAL,
 			spacing: 15,
 			margin: 6,
 		});
 		colsBox.pack_start(new Gtk.Label({ label: nbColsLabel, halign: Gtk.Align.START }), false, false, 0);
-		colsBox.pack_end(nbCols, false, false, 0);
-	
+		colsBox.pack_end(nbColsSpinBtn, false, false, 0);
+
 		//---------------------------------------------------
-		
+
 		let keybindingBox = new Gtk.Box({
 			orientation: Gtk.Orientation.VERTICAL,
 			spacing: 5,
 			tooltip_text: _("Default value is") +  " <Super>e" +'\n'+ RELOAD_TEXT
 		});
-		
+
 		let keybindingEntry = new Gtk.Entry({
 			sensitive: SETTINGS.get_boolean('use-keybinding'),
 			hexpand: true
 		});
-		
+
 		if (SETTINGS.get_strv('emoji-keybinding') != '') {
 			keybindingEntry.text = SETTINGS.get_strv('emoji-keybinding')[0];
 		}
-		
+
+		SETTINGS.connect('changed::emoji-keybinding', () => {
+			keybindingEntry.set_text(SETTINGS.get_strv('emoji-keybinding')[0]);
+		});
+
 		let keybindingButton = new Gtk.Button({ sensitive: SETTINGS.get_boolean('use-keybinding'), label: _("Apply") });
-		
-		keybindingButton.connect('clicked', Lang.bind(this, function(widget) {
+
+		keybindingButton.connect('clicked', widget => {
 			SETTINGS.set_strv('emoji-keybinding', [keybindingEntry.text]);
-		}));
+		});
 		let keybindingBox1 =  new Gtk.Box({
 			orientation: Gtk.Orientation.HORIZONTAL,
 			spacing: 15,
 			margin: 6,
 		});
-		
+
 		//---------------------------------------------------------------
-		
+
 		let keybindingLabel = _("Use a keyboard shortcut to toggle the menu:");
-		
+
 		let keybindingSwitch = new Gtk.Switch();
 		keybindingSwitch.set_state(true);
 		keybindingSwitch.set_state(SETTINGS.get_boolean('use-keybinding'));
-		
-		keybindingSwitch.connect('notify::active', Lang.bind(this, function(widget) {
+
+		keybindingSwitch.connect('notify::active', widget => {
 			if (widget.active) {
 				SETTINGS.set_boolean('use-keybinding', true);
 				keybindingEntry.sensitive = true;
@@ -270,8 +286,12 @@ function buildPrefsWidget() {
 				alwaysShowSwitch.set_state(true);
 				alwaysShowSwitch.sensitive = false;
 			}
-		}));
-		
+		});
+
+		SETTINGS.connect('changed::use-keybinding', () => {
+			keybindingSwitch.set_state(SETTINGS.get_boolean('use-keybinding'));
+		});
+
 		let keybindingBox2 =  new Gtk.Box({
 			orientation: Gtk.Orientation.HORIZONTAL,
 			spacing: 15,
@@ -279,83 +299,87 @@ function buildPrefsWidget() {
 		});
 		keybindingBox2.pack_start(new Gtk.Label({ label: keybindingLabel, halign: Gtk.Align.START }), false, false, 0);
 		keybindingBox2.pack_end(keybindingSwitch, false, false, 0);
-		
+
 		keybindingBox.pack_start(keybindingBox2, false, false, 0);
 		keybindingBox1.pack_start(keybindingEntry, true, true, 0);
 		keybindingBox1.pack_end(keybindingButton, false, false, 0);
 		keybindingBox.pack_end(keybindingBox1, false, false, 0);
-		
+
 		//-------------------------------------------------------
-		
+
 		let alwaysShowLabel = _("Always show the icon:");
 		let alwaysShowSwitch = new Gtk.Switch();
 		alwaysShowSwitch.set_state(true);
 		alwaysShowSwitch.set_state(SETTINGS.get_boolean('always-show'));
-		
-		alwaysShowSwitch.connect('notify::active', Lang.bind(this, function(widget) {
+
+		alwaysShowSwitch.connect('notify::active', widget => {
 			if (widget.active) {
 				SETTINGS.set_boolean('always-show', true);
 			} else {
 				SETTINGS.set_boolean('always-show', false);
 			}
-		}));
-		
+		});
+
+		SETTINGS.connect('changed::always-show', () => {
+			alwaysShowSwitch.set_state(SETTINGS.get_boolean('always-show'));
+		});
+
 		let alwaysShowBox = new Gtk.Box({
 			orientation: Gtk.Orientation.HORIZONTAL,
 			spacing: 15,
 			margin: 6,
-//			tooltip_text: _(""),
+			tooltip_text: _("If you access the menu with the keyboard shorcut, you might not need to always display the icon."),
 		});
 		alwaysShowBox.pack_start(new Gtk.Label({ label: alwaysShowLabel, halign: Gtk.Align.START }), false, false, 0);
 		alwaysShowBox.pack_end(alwaysShowSwitch, false, false, 0);
-		
+
 		//-------------------------------------------------------
-	
+
 	settingsPage.add_row(colsBox, layoutSection);
 	settingsPage.add_row(positionBox, layoutSection);
 	settingsPage.add_row(lightThemeBox, appearanceSection);
 	settingsPage.add_row(sizeBox, appearanceSection);
-	settingsPage.add_row(keybindingBox, keybindingSection);	
-	settingsPage.add_row(alwaysShowBox, keybindingSection);	
-	
+	settingsPage.add_row(keybindingBox, keybindingSection);
+	settingsPage.add_row(alwaysShowBox, keybindingSection);
+
 	//------------------------------------
 
 	let aboutPage = widget.add_page("about", _("About"));
-		
+
 		let a_name = '<b>' + Me.metadata.name.toString() + '</b>';
 		let a_uuid = Me.metadata.uuid.toString();
-		let a_description = _(Me.metadata.description.toString());
-		
+		let a_description = Me.metadata.description.toString();
+
 		let label_name = new Gtk.Label({ label: a_name, use_markup: true, halign: Gtk.Align.CENTER });
-		
+
 		let a_image = new Gtk.Image({ pixbuf: GdkPixbuf.Pixbuf.new_from_file_at_size(Me.path+'/icons/about_icon.png', 128, 128) });
 		let label_description = new Gtk.Label({ label: a_description, wrap: true, halign: Gtk.Align.CENTER });
-		
+
 		let label_contributors = new Gtk.Label({
-			label: "Author: roschan. Contributors: amivaleo, picsi, jonnius",
+			label: "Author: Romain F.T.; Contributors: amivaleo, picsi, jonnius",
 			wrap: true,
 			halign: Gtk.Align.CENTER
 		});
-		
+
 		let about_box = new Gtk.Box({ orientation: Gtk.Orientation.VERTICAL, spacing: 10});
 		about_box.pack_start(label_name, false, false, 0);
 		about_box.pack_start(a_image, false, false, 0);
 		about_box.pack_start(label_description, false, false, 0);
 		about_box.pack_start(label_contributors, false, false, 0);
-		
+
 		let LinkBox = new Gtk.Box({ orientation: Gtk.Orientation.HORIZONTAL, spacing: 10 });
 		let a_version = ' (v' + Me.metadata.version.toString() + ') ';
-		
+
 		let url_button = new Gtk.LinkButton({
 			label: _("Report bugs or ideas"),
 			uri: Me.metadata.url.toString()
 		});
-		
+
 		LinkBox.pack_start(url_button, false, false, 0);
 		LinkBox.pack_end(new Gtk.Label({ label: a_version, halign: Gtk.Align.START }), false, false, 0);
-		
+
 		aboutPage.stackpageMainBox.pack_end(LinkBox, false, false, 0);
-	
+
 	aboutPage.add_widget(about_box);
 
 	//----------------------------------------------------
@@ -363,12 +387,22 @@ function buildPrefsWidget() {
 	Mainloop.timeout_add(0, () => {
 		let headerBar = widget.get_toplevel().get_titlebar();
 		headerBar.custom_title = widget.switcher;
+
 		return false;
 	});
 
 	widget.show_all();
-	
 	return widget;
+}
+
+function reset_settings(b) {
+	SETTINGS.reset('emojisize');
+	SETTINGS.reset('light-theme');
+	SETTINGS.reset('nbcols');
+	SETTINGS.reset('position');
+	SETTINGS.reset('emoji-keybinding');
+	SETTINGS.reset('use-keybinding');
+	SETTINGS.reset('always-show');
 }
 
 
