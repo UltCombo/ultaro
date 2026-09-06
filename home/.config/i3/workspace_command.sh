ccc8818d363c52a9de2adf21e9d4375233a33007
UltCombo <ultcombo@gmail.com>
2026-09-06 15:11:13 -0300

    chore: add debug log

--- home/.config/i3/workspace_command.sh
+++ home/.config/i3/workspace_command.sh
@@ -5,4 +5,4 @@ set -o pipefail
 i3-msg -t subscribe -m '["workspace", "output"]' | {
     i3-msg -t get_workspaces;
     while read EVENT; do i3-msg -t get_workspaces; done;
-} | jq --unbuffered -c '[ .[] | select(.name != "Private") ]' || exec "$0" "$@"
+} | jq --unbuffered -c '[ .[] | select(.name != "Private") ]' || echo "workspace command crashed" >> /tmp/ultaro.log && exec "$0" "$@"
